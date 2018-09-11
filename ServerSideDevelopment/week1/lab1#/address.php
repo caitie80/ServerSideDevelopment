99482d8d63aff6dabfbf83bbb275d79567193acb
Caitie <caitie.n.pearce@students.ittralee.ie>
2018-09-11 15:33:36 +0100

    Add files via upload
    week 1 lab 1

---
+++ ServerSideDevelopment/week1/lab1#/address.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title></title>
+    <meta charset='utf-8'>
+  </head>
+  <body>
+  
+        <?php
+            echo "<h1>Caitie Pearce</h1>
+                  <h2>Alderwood Rd,</h2>
+                  <h3>Tralee,</h3>
+                  <h4>Co. Kerry</h4>";
+        
+        
+        ?>
+
+  </body>
+</html>
---
+++ ServerSideDevelopment/week1/lab1#/hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title></title>
+    <meta charset='utf-8'>
+    
+  </head>
+  
+  <body>
+        <?php
+        echo "<h1>Hello world</h1>";
+        
+        ?>
+        <?php
+        echo "<p>today's date is " . date("l, F dS Y.") . "</p>"; 
+         
+        ?>
+        
+  </body>
+</html>
